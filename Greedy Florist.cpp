03ad928d345fedd31e216733ead46d4808dc0fbd
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-08-05 12:15:31 +0900

    Update Greedy Florist.cpp

--- Greedy Florist.cpp
+++ Greedy Florist.cpp
@@ -1,1 +1,12 @@
-
+int getMinimumCost(int k, vector<int> c)
+{
+    std::sort(c.begin(), c.end(), greater<int>());
+    int result = 0;
+    
+    for (size_t i = 0; i < c.size(); ++i)
+    {
+        result += c[i] * (floor(i / k) + 1);
+    }
+    
+    return result;
+}
