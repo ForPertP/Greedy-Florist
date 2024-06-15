f05bb52f35c000045310864d31cf660b071c647d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-06-16 05:28:45 +0900

    Update Greedy-Florist.cs

--- Greedy-Florist.cs
+++ Greedy-Florist.cs
@@ -16,6 +16,16 @@ class Solution {
 
     static int getMinimumCost(int k, int[] c)
     {
+        int result = 0;
+
+        Array.Sort(c, (a, b) => b.CompareTo(a));
+
+        for (int i = 0; i < c.Length; i++)
+        {
+            result += c[i] * ((int)Math.Floor((double)i / k) + 1);
+        }
+
+        return result;
     }
 
 
