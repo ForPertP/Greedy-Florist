80daa9d41af8ab2ce5410f75ee0ff6eeb2f8efcf
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-06-18 07:23:50 +0900

    Update Greedy-Florist.java

--- Greedy-Florist.java
+++ Greedy-Florist.java
@@ -7,15 +7,28 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-
     static int getMinimumCost(int k, int[] c) {
         int result = 0;
+
+        Arrays.sort(c);
+
+        for (int i = 0, j = c.length - 1; i < j; i++, j--) {
+            int temp = c[i];
+            c[i] = c[j];
+            c[j] = temp;
+        }
+
+        for (int i = 0; i < c.length; i++) {
+            result += c[i] * (Math.floor(i / (double)k) + 1);
+        }
 
         return result;
     }
 
+    
     private static final Scanner scanner = new Scanner(System.in);
 
+    
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
